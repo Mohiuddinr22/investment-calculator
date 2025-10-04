703ff38eb1c524ebf75f273cd5bc7ddd22982382
Mohiuddin Mohi <mohiuddinr.22@gmail.com>
2025-10-04 08:40:40 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+import { useState } from "react";
+
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const isValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
+  return (
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {isValid ? (
+        <Results input={userInput} />
+      ) : (
+        <p className="center">please use a valid input</p>
+      )}
+    </>
+  );
+}
+
+export default App;
